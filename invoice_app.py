1f590842f82c9a2defbedba300e289627244c0e6
youclinic <yunusemreyoustemcell@gmail.com>
2025-03-27 21:52:06 +0300

    Update invoice_app.py

--- invoice_app.py
+++ invoice_app.py
@@ -123,5 +123,5 @@ def index():
 
     return render_template("invoice_form.html")
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
